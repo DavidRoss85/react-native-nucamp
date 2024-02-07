11effd910ce88ef32b0c6fc987eaf17cfe83cab2
David Ross <officialdavidross@gmail.com>
2024-02-07 09:33:01 -1000

    Favorite icon

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,9 +1,9 @@
 import { ScrollView, Text } from "react-native";
-import { Card, ListItem } from "react-native-elements";
+import { Avatar, Card, ListItem } from "react-native-elements";
 import { PARTNERS } from "../shared/partners";
 import { useState } from "react";
 
-const AboutScreen = () =>{
+const AboutScreen = () => {
 
     const [partners, setPartners] = useState(PARTNERS);
 
@@ -12,11 +12,11 @@ const AboutScreen = () =>{
             <Mission />
             <Card>
                 <Card.Title>Community Partners</Card.Title>
-                <Card.Divider/>
-                {partners.map((partner)=>{
-                    return(
+                <Card.Divider />
+                {partners.map((partner) => {
+                    return (
                         <ListItem key={partner.id}>
-                            <Avatar source={partner.image} rounded/>
+                            <Avatar source={partner.image} rounded />
                             <ListItem.Content>
                                 <ListItem.Title>{partner.name}</ListItem.Title>
                                 <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
@@ -29,13 +29,13 @@ const AboutScreen = () =>{
     );
 };
 
-const Mission = ()=>{
-    
+const Mission = () => {
+
     return (
         <Card>
             <Card.Title>Our Mission</Card.Title>
-            <Card.Divider/>
-            <Text style={{margin:10}}>
+            <Card.Divider />
+            <Text style={{ margin: 10 }}>
                 We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
             </Text>
         </Card>
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -6,30 +6,35 @@ import { COMMENTS } from '../shared/comments';
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const [comments, setComments] = useState(COMMENTS);
+    const [favorite, setFavorite]=useState(false);
 
-    const renderCommentItem = ( {item}) =>{
-        return(
+    const renderCommentItem = ({ item }) => {
+        return (
             <View style={StyleSheet.commentItem}>
-                <Text style={{ fontSize: 14}}>{item.text}</Text>
-                <Text style={{ fontSize: 12}}>{item.rating} Stars</Text>
-                <Text style={{ fontSize: 12}}>{`--${item.author}`}</Text>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>{`--${item.author}`}</Text>
             </View>
         )
     }
     return (
-        <FlatList 
-            data={comments.filter((comment)=>{
-                comment.campsiteId===campsite.id
-            })}
+        <FlatList
+            data={comments.filter(
+                (comment) => comment.campsiteId === campsite.id
+            )}
             renderItem={renderCommentItem}
-            keyExtractor={(item)=>item.id.toString()}
+            keyExtractor={(item) => item.id.toString()}
             contentContainerStyle={{
                 marginHorizontal: 20,
                 paddingVertical: 20
             }}
             ListHeaderComponent={
                 <>
-                    <RenderCampsite campsite={campsite} />
+                    <RenderCampsite 
+                        campsite={campsite} 
+                        isFavorite={favorite}
+                        markFavorite={()=>setFavorite(true)}
+                    />
                     <Text style={StyleSheet.commentsTitle}>Comments</Text>
                 </>
             }
@@ -38,18 +43,18 @@ const CampsiteInfoScreen = ({ route }) => {
 };
 
 const style = StyleSheet.create({
-    commentsTitle:{
+    commentsTitle: {
         textAlign: 'center',
         backgroundColor: '#fff',
         fontSize: 16,
         fontWeight: 'bold',
         color: '#43484D',
         padding: 10,
-        paddingTop:30
+        paddingTop: 30
     },
-    commentItem:{
-        paddingVertical:10,
-        paddingHorizontal:20,
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
         backgroundColor: '#fff'
     }
 })
